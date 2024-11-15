0f4a688b9535802f34ad7f63b62c57228f42b09e
JohnKamaujk <johnjkkamau@gmail.com>
2024-11-15 22:38:01 +0300

    :wrench: database configuration

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,5 +1,14 @@
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
+
 export default {
   dialect: "postgresql",
   schema: "./utils/db/schema.ts",
   out: "./drizzle",
+
+  dbCredentials: {
+    url: process.env.DB_URL,
+    connectionString: process.env.DB_URL,
+  },
 };
