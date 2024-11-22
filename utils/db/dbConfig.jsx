5948225d2e62f06c237a86290d546dfd26d4bfd1
JohnKamaujk <johnjkkamau@gmail.com>
2024-11-22 22:21:49 +0300

    :sparkles:feat: database configuration

--- utils/db/dbConfig.jsx
+++ utils/db/dbConfig.jsx
@@ -1,9 +1,20 @@
 import { neon } from "@neondatabase/serverless";
-
 import { drizzle } from "drizzle-orm/neon-http";
-
 import * as schema from "./schema";
 
-const sql = neon(process.env.DB_URL);
+function createDb() {
+  if (!process.env.NEXT_PUBLIC_DATABASE_URL) {
+    throw new Error("DATABASE_URL is missing");
+  }
 
-export const db = drizzle(sql, { schema });
+  try {
+    const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
+    console.log("Successfully initialized Neon connection.");
+    return drizzle(sql, { schema });
+  } catch (error) {
+    console.error("Error initializing Neon or Drizzle:", error);
+    throw error;
+  }
+}
+
+export const db = createDb(); 
